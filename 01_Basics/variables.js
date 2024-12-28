d0cdcfba694303de4c2d5adb3345945a16d48ce1
Ritiksaini143-debug <sainiritik62697@gmail.com>
2024-12-28 05:03:47 +0000

    variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,28 @@
+const accountId = 123456;
+
+let accountEmail = "sainiritik123@gmail.com";
+
+var accountPassword = "12345";
+
+accountCity = "uttar pradesh";
+
+let accountState; // undefined data type
+
+// accountId = 4321 not allowed const value can't be changed.
+
+accountEmail = "ritiksaini@gmail.com";
+
+accountPassword = "323232";
+
+accountCity = "uttarakhand";
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
