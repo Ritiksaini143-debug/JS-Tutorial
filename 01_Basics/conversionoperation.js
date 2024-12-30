a7448909a00c516aea120b3598023c360918d694
Ritiksaini143-debug <sainiritik62697@gmail.com>
2024-12-30 04:57:15 +0000

    conversionoperation

---
+++ 01_Basics/conversionoperation.js
@@ -0,0 +1,82 @@
+let score = "33aaa";
+
+//  console.log(typeof score);
+
+//  console.log(typeof(score))
+
+let newValue = Number(score); //convert string into number
+
+//  console.log( typeof newValue);
+
+//  console.log(newValue);
+
+/*
+"33" = 33
+"33abc" = NaN  (not an number)
+
+true = 1
+false = 0
+
+undefined = NaN
+
+*/
+
+let isLogedIn = 1;
+
+let booleanisLoggedIn = Boolean(isLogedIn);
+
+// console.log(booleanisLoggedIn);
+//  console.log(typeof booleanisLoggedIn);
+
+// 1 = true;  0 = false;
+// " " = false
+// "ritik" = true;
+
+let someNumber = 22;
+
+let stringNumber = String(someNumber);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let value = 5;
+let newValue1 = -value;
+// console.log(newValue1);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(5 % 2);
+
+let str1 = "hello";
+let str2 = " Ritik";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// postfix
+
+let a = 5;
+
+let b = a++;
+
+// console.log(a); a=6
+
+// console.log(b); b=5
+
+// prefix
+
+let x = 5;
+
+let y = ++x;
+
+ console.log(x);  x=6
+
+ console.log(y);  y=6
